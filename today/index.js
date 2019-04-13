87ae7afce281212b10116a06ef0d5bf9ea54be90
Jens Walter <jens@apimeister.com>
2019-04-13 14:47:38 -0700

    fix substring for date pattern

--- today/index.js
+++ today/index.js
@@ -6,7 +6,7 @@ exports.handler = (event, context,callback) => {
   console.log("context");
   console.log(JSON.stringify(context,null,2));
   let date = new Date().toISOString();
-  let result={"today":date.substring(1,10)};
+  let result={"today":date.substring(0,10)};
   console.log(JSON.stringify(result,null,2));
   callback(null,result);
 }
