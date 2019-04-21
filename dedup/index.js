8f310a7e38f3b6fc9d69f96d7fcc77cc7d866cd5
Jens Walter <jens@apimeister.com>
2019-04-21 22:28:53 +0200

    fix direct call without lastvalue

--- dedup/index.js
+++ dedup/index.js
@@ -1,6 +1,8 @@
 exports.handler = (event, context,callback) => {
   event.dedupid=JSON.stringify(event);
-  if(context.clientContext.custom.lastvalue == event.dedupid){
+  if(context.clientContext && context.clientContext.custom
+    && context.clientContext.custom.lastvalue
+    && context.clientContext.custom.lastvalue == event.dedupid){
     callback(null, {});
   }else{
     callback(null, event);
